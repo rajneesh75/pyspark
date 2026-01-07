7801ea68a6651f2eff7baafb94d62056422ec621
rajneesh <rajneesh75@yahoo.com>
2026-01-07 15:14:59 +0000

    commit

--- deltalake5.py
+++ deltalake5.py
@@ -1,33 +1,27 @@
 from pyspark.sql import SparkSession
-from delta.tables import DeltaTable
 
-builder = (
+spark = (
     SparkSession.builder
-    .appName("DeltaSpark4")
+    .appName("test")
+    .enableHiveSupport()
     .config("spark.jars.packages", "io.delta:delta-spark_2.13:4.0.0")
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    .config("spark.sql.warehouse.dir", "./spark-warehouse")
+    .getOrCreate()
 )
-
-spark = builder.getOrCreate()
-delta_path = "/home/rajneesh/deltalake/employees"
-
 
 print("Reading from deltalake")
 
-delta_df = spark.read.format("delta").load(delta_path)
+delta_df = spark.read.format("delta").table("employees")
 delta_df.show()
-
-print("Creating Delta Table object")
-delta_table = DeltaTable.forPath(spark, delta_path)
-delta_table.toDF().show()
 
 print("Registering Delta Table as SQL Temporary View")
 delta_df.createOrReplaceTempView("people")
 spark.sql("""SELECT * FROM people""").show()
 
 print("Saving Delta Table as SQL Table")
-delta_df.write.mode("overwrite").saveAsTable("employees")
+delta_df.write.mode("overwrite").saveAsTable("people")
 
 print("Querying Delta Table as SQL Table")
-spark.sql("""SELECT * FROM employees""").show()
+spark.sql("""SELECT * FROM people""").show()
--- deltalakemanagedtables1.py
+++ deltalakemanagedtables1.py
@@ -1,14 +1,15 @@
 from pyspark.sql import SparkSession
 
-builder = (
+spark = (
     SparkSession.builder
-    .appName("DeltaSpark4")
+    .appName("test")
+    .enableHiveSupport()
     .config("spark.jars.packages", "io.delta:delta-spark_2.13:4.0.0")
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    .config("spark.sql.warehouse.dir", "./spark-warehouse")
+    .getOrCreate()
 )
-
-spark = builder.getOrCreate()
 
 print(spark)
 
