eec60ce5ea2291c09ae81a446038b915d5027d48
rajneesh <rajneesh75@yahoo.com>
2026-07-06 15:06:57 +0000

    commit

--- pyspark6.py
+++ pyspark6.py
@@ -12,7 +12,6 @@ square_udf = udf(square, IntegerType())
 spark = (
     SparkSession.builder
     .appName("test")
-    .enableHiveSupport()
     .config("spark.jars.packages", "io.delta:delta-spark_2.13:4.0.0")
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
--- pyspark7.py
+++ pyspark7.py
@@ -3,7 +3,6 @@ from pyspark.sql import SparkSession
 spark = (
     SparkSession.builder
     .appName("test")
-    .enableHiveSupport()
     .config("spark.jars.packages", "io.delta:delta-spark_2.13:4.0.0")
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
@@ -22,6 +21,5 @@ data = [
 columns = ["id", "name", "age"]
 
 df = spark.createDataFrame(data, columns)
-#df.explain(True)
 df.show()
 df.printSchema()
